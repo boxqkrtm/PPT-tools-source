185f43c0bb8145b1aba243cc6674d2444f1423fd
whitetac <9902abcd@gmail.com>
2020-09-08 10:49:40 +0900

    Add files via upload

---
+++ PPC-ArleSkin/PPC-ArleSkin/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPC_ArleSkin
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread worker;
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+            InitializeComponent();
+            worker = new Thread(() => Run());
+            worker.Start();
+        }
+        private void Run()
+        {
+            while (true)
+            {
+                ProcessMemory Game = new ProcessMemory("PuyoPuyoChampions");
+                Game.WriteInt32(new IntPtr(0x7FF603333AF8), 26);
+                Thread.Sleep(10);
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (worker != null) worker.Abort();
+        }
+    }
+}
